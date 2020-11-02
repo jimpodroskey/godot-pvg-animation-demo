7e73ef7629f37a048c1620ccc5dd6baa67c84a51
Jim Podroskey <jimmyp@hey.com>
2020-11-02 06:04:56 -0500

    Re-commit after second record

--- Chicken.gd
+++ Chicken.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 100
 
 var target = Vector2()
 var velocity = Vector2(1,0)
@@ -8,15 +8,16 @@ var velocity = Vector2(1,0)
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get('parameters/playback')
 
+
 func _ready():
 	animationTree.active = true
-	set_blend_positions()
-
-	
 
 func _physics_process(_delta):
 	if Input.is_action_pressed('mouse_walk'):
 		target = get_global_mouse_position()
+		animationTree.set('parameters/Idle/blend_position', velocity.normalized())
+		animationTree.set('parameters/Walk/blend_position', velocity.normalized())
+		animationTree.set('parameters/Attack/blend_position', velocity.normalized())
 	else:
 		target = position
 
@@ -24,16 +25,11 @@ func _physics_process(_delta):
 	velocity = position.direction_to(target) * speed
 
 	if position.distance_to(target) > 5:
-		velocity = move_and_slide(velocity)
 		animationState.travel("Walk")
-		set_blend_positions()
+		velocity = move_and_slide(velocity)
 	else:
 		animationState.travel("Idle")
 
 	if Input.is_action_just_pressed("attack"):
 		animationState.travel("Attack")
 
-func set_blend_positions():
-	animationTree.set('parameters/Idle/blend_position', velocity.normalized())
-	animationTree.set('parameters/Walk/blend_position', velocity.normalized())
-	animationTree.set('parameters/Attack/blend_position', velocity.normalized())
